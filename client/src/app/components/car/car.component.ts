f4583e53fe5977a56bcc964e45f0ff43ef1c9f9a
danoprica <danutzoprica@yahoo.com>
2023-11-08 14:14:30 +0200

    Car component show data from DB

--- client/src/app/components/car/car.component.ts
+++ client/src/app/components/car/car.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {faChevronUp, faEdit, faPlus, faTrashAlt} from "@fortawesome/free-solid-svg-icons";
+import axios from "axios";
 
 @Component({
   selector: 'app-car',
@@ -7,10 +8,20 @@ import {faChevronUp, faEdit, faPlus, faTrashAlt} from "@fortawesome/free-solid-s
   styleUrls: ['./car.component.scss']
 })
 export class CarComponent implements OnInit {
+  cars: any = [];
+  limit: number = 70;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.loadData()
+  }
+
+  loadData = (): void => {
+    axios.get('/api/car').then(({ data }) => {
+      this.cars = data;
+    })
+
   }
 
   protected readonly faChevronUp = faChevronUp;
